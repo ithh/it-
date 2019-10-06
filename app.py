9fe81395b065de571b37ca1609effb341a9fbd57
hui <1187175000@qq.com>
2019-10-06 17:25:24 +0800

    优化注册、登陆和主页面 完善后台页面跳转 编写注册页面后台

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
+
 from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@127.0.0.1:3306/mydb?charset=utf8'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Student(db.Model):
+    __tablename__ = "student"
+    sno = db.Column(db.Integer, primary_key=True)
+    sname = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, sname, email):
+        self.sname = sname
+        self.email = email
+
+    def __repr__(self):
+        return '<Student %r>' % self.sname
 
 @app.route("/")
 def index_handle():
@@ -16,6 +37,15 @@ def login_handle():
 def reg_handle():
     if request.method == "GET":
         return render_template("register.html")
+    elif request.method == "POST":
+        sname = request.form.get("sname")
+        email = request.form.get("email")
+        print(sname, email)
+        stu = Student(sname, email)
+        db.session.add(stu)
+        db.session.commit()
+
+        return "注册成功"
 
 
 if __name__ == '__main__':
