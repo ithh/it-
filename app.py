31d9a29cd54862905b69a8cc939f25da01d09de9
hui <1187175000@qq.com>
2019-10-07 17:18:17 +0800

    编写注册后台，创建数据库，增加用户信息表单 编写个人帖子页面， 优化注册、登陆和首页页面

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 
+
 app = Flask(__name__)
 
 
@@ -12,36 +13,50 @@ db = SQLAlchemy(app)
 
 class Student(db.Model):
     __tablename__ = "student"
-    sno = db.Column(db.Integer, primary_key=True)
-    sname = db.Column(db.String(80), unique=True)
+    # sno = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    userName = db.Column(db.String(80), primary_key=True, unique=True)
+    password = db.Column(db.String(64))
     email = db.Column(db.String(120), unique=True)
 
-    def __init__(self, sname, email):
-        self.sname = sname
+    def __init__(self, userName, password, email):
+        self.userName = userName
+        self.password = password
         self.email = email
 
-    def __repr__(self):
-        return '<Student %r>' % self.sname
+    # def __repr__(self):
+    #     return '<Student %r>' % self.userName
+    # def __iter__(self):
+    #     return '<Student %r>' % self.userName
+# db.create_all()
+
 
 @app.route("/")
 def index_handle():
     if request.method == "GET":
         return render_template("it_tieba.html")
 
-@app.route("/login")
+@app.route("/login", methods=['POST','GET'])
 def login_handle():
     if request.method == "GET":
         return render_template("login.html")
+    # elif request.method == "POST":
+    #     userName = request.form.get("userName")
+    #     password = request.form.get("password")
 
-@app.route("/register")
+
+@app.route("/register", methods=['POST','GET'])
 def reg_handle():
     if request.method == "GET":
         return render_template("register.html")
     elif request.method == "POST":
-        sname = request.form.get("sname")
+        userName = request.form.get("userName")
+        # result = Student.query.filter(Student.userName == "%s" , (userName,)).first()
+        # if result == 0:
+        #     return "用户名已注册"
+        password = request.form.get("password")
         email = request.form.get("email")
-        print(sname, email)
-        stu = Student(sname, email)
+        # print(userName,password, email)
+        stu = Student(userName, password, email)
         db.session.add(stu)
         db.session.commit()
 
